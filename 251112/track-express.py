067afcdf9de39373e3753fe2db25fd7ffb955260
kenichiLyon <128369797+kenichiLyon@users.noreply.github.com>
2025-11-12 18:18:50 +0800

    fix typo

--- 251112/track-express.py
+++ 251112/track-express.py
@@ -3,6 +3,6 @@ TotalWeight = 29.5
 BigTruck = 4.0
 SmallTruck = 2.5
 LeftWeight = TotalWeight - BigTruck * 3 + SmallTruck
-print('剩余的重量:', LeftWeight, '吨')
+print('剩余的重量：', LeftWeight, '吨')
 Times = LeftWeight / SmallTruck
-print('还需要:', int(Times), '次，才能运完')
+print('还需要：', int(Times), '次，才能运完')
