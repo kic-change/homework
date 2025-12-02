d8c2e2392da143ee679a0b6fe496b928d7d85406
kenichiLyon <128369797+kenichiLyon@users.noreply.github.com>
2025-12-03 00:01:20 +0800

    via 251203 upload

---
+++ 251202/test.py
@@ -0,0 +1,21 @@
+# x = 7
+# y = 13
+# x += y 
+# y = x - y
+# x -= y
+# print(x)
+# print(y)
+
+# import re
+
+# text = input('please input something here: ')
+# stats ={
+#         'Chinese':len(re.findall(r'[\u4e00-\u9fff]',text)),
+#         'English':len(re.findall(r'[a-zA-Z]',text)),
+#         'Number':len(re.findall(r'\d',text)),
+#         'Space':len(re.findall(r'\s',text)),
+#         'Other':len(text)-len(re.findall(r'[\u4e00-\u9fff]|[a-zA-Z]|\d|\s',text)),
+#     }
+# print('\n 统计结果：')
+# for key,value in stats.items():
+#         print(f'{key}:{value}')
